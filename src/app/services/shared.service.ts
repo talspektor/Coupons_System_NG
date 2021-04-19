90bb128951a7bab3168bb0ecf9d4efee68bdc08b
Tal <talspektor.gmail.com>
2021-04-19 07:52:52 +0300

    fix

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -1,21 +1,35 @@
 import { Observable, Observer } from 'rxjs';
 import { Injectable } from '@angular/core';
+import { ClientType } from '../models/client-type.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SharedService {
+
   public token: string = '';
-  public globalVarUpdate?: Observable<String>;
-  public globalVarObserver?: Observer<String>;
+  public clientType?: ClientType
+  public tokenUpdate?: Observable<String>;
+  public tokenObserver?: Observer<String>;
+  public clientObserver?: Observer<ClientType>;
+  public clientUpdate?: Observable<ClientType>;
+
   constructor() {
-    // this.token = Observable.create((observer: Observer<String>) => {
-    //   this.globalVarObserver = observer;
-    // });
+    this.token = Observable.create((observer: Observer<String>) => {
+      this.tokenObserver = observer;
+    });
+    this.clientType = Observable.create((observer: Observer<ClientType>)=>{
+      this.clientObserver = observer
+    })
   }
 
-  // public updateGlobalVar(newValue: string) {
-  //   this.token = newValue;
-  //   this.globalVarObserver?.next(this.token);
-  // }
+  public updateToken(newValue: string) {
+    this.token = newValue;
+    this.tokenObserver?.next(this.token);
+  }
+
+  public updateClientType(newValue: ClientType) {
+    this.clientType = newValue;
+    this.clientObserver?.next(this.clientType)
+  }
 }
